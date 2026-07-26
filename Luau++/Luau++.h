38515e2fec5e97104f23fa15c682aca129f7292e
Cashierob <hoangduyphuoc3@gmail.com>
2026-07-26 13:18:48 +0700

    Update Luau++.h

--- Luau++/Luau++.h
+++ Luau++/Luau++.h
@@ -1,3 +1,14 @@
+/*
+ * Copyright 2026 [@Cashierob]
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://apache.org
+ */
+
+
 #ifndef LUAU_H
 #define LUAU_H
 

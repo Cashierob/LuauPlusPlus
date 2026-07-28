f199feb61eae47ccb17f2fa2c48f962b5856c74d
Cashierob <hoangduyphuoc3@gmail.com>
2026-07-28 09:44:14 +0700

    Update copyright and C++ version note
    Updated copyright information and added a note about C++ version compatibility.

--- Luau++/Luau++.h
+++ Luau++/Luau++.h
@@ -12,6 +12,8 @@
 //See the License for the specific language governing permissions and
 //limitations under the License.
 
+//code is only compile-able for C++ version 20 to above.
+
 
 #ifndef LUAU_H
 #define LUAU_H

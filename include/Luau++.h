6eb1199062cd89ca7f1bfada92ea849fef237a61
Cashierob <hoangduyphuoc3@gmail.com>
2026-07-28 13:25:58 +0700

    Refactor Luau header for clarity and error handling
    Updated copyright information and improved error handling in LocalDataStoreService methods.

--- include/Luau++.h
+++ include/Luau++.h
@@ -12,10 +12,10 @@
 //See the License for the specific language governing permissions and
 //limitations under the License.
 
-//This header is only compliable from C++ version 20 to above
-//Compile your CPP file with "g++ -std=c++20 Yourfilename.cpp -o Name"
+//Only compatible with C++ Version 20 or above.
+//Compile your CPP file with "g++ -std=c++20 Yourfilename.cpp -o Name".
 
-
+#pragma once
 #ifndef LUAU_H
 #define LUAU_H
 
@@ -34,7 +34,6 @@
 #include <type_traits>
 
 
-
 inline void printl(const auto& message) {
     std::cout << message << std::endl;
 }
@@ -45,7 +44,7 @@ inline std::string UserInput() {
     return input;
 }
 
-inline std::string typefl(const auto& input) {
+inline std::string typefl(const auto& input) noexcept {
     if constexpr (std::is_same_v<std::decay_t<decltype(input)>, bool>) {
         return "bool";
     } else if constexpr (std::is_integral_v<std::decay_t<decltype(input)>>) {
@@ -62,7 +61,7 @@ inline std::string typefl(const auto& input) {
 }
 
 namespace taskl {
-    inline void waitl(double duration) {
+    inline void waitl(double duration) noexcept {
         if (typefl(duration) == "double") {
             std::this_thread::sleep_for(std::chrono::milliseconds(static_cast<unsigned int>(std::floor(duration) * 1000)));
         } else if (typefl(duration) == "int") {
@@ -98,7 +97,7 @@ namespace stringl {
 };
 
 namespace mathl {
-    inline int randoml(int min, int max) {
+    inline int random(int min, int max) {
         std::random_device rd;
         std::mt19937 gen(rd());
         std::uniform_int_distribution<> dis(min, max);
@@ -113,35 +112,35 @@ namespace mathl {
         return std::numeric_limits<double>::infinity();
     }
 
-    inline double absl(double value) {
+    inline double absl(double value) noexcept {
         return std::abs(value);
     }
 
-    inline int ceill(double value) {
+    inline int ceill(double value) noexcept {
         return static_cast<int>(std::ceil(value));
     }
 
-    inline int floorl(double value) {
+    inline int floorl(double value) noexcept {
         return static_cast<int>(std::floor(value));
     }
 
-    inline double roundl(double value) {
+    inline double roundl(double value) noexcept {
         return std::round(value);
     }
 
-    inline double sqrtl(double value) {
+    inline double sqrtl(double value) noexcept {
         return std::sqrt(value);
     }
 
-    inline double powl(double base, double exponent) {
+    inline double powl(double base, double exponent) noexcept {
         return std::pow(base, exponent);
     }
 
-    inline double fmodl(double value, int num) {
+    inline double fmodl(double value, int num) noexcept {
         return std::fmod(value, static_cast<double>(num));
     }
 
-    inline double clampl(double value, double min, double max) {
+    inline double clampl(double value, double min, double max) noexcept {
         if (value < min) {
             return min;
         } else if (value > max) {
@@ -150,79 +149,158 @@ namespace mathl {
         return value;
     }
 
-    inline double sinl(double value) {
+    inline double sinl(double value) noexcept {
         return std::sin(value);
     }
 
-    inline double cosl(double value) {
+    inline double cosl(double value) noexcept {
         return std::cos(value);
     }
 
-    inline double tan(double value) {
+    inline double tanl(double value) noexcept {
         return std::tan(value);
     }
 
     template<typename... Args>
-    auto max(Args... args)
+    auto maxl(Args... args) noexcept
     {
         return std::max({args...});
     }
 
     template<typename... Args>
-    auto min(Args... args)
+    auto minl(Args... args) noexcept
     {
         return std::min({args...});
     }
 };
 
 namespace LocalDataStoreService {
-    inline void GetLocalDataStore(const std::string& name) {
-        std::filesystem::path Database = "./LocalDataStore";
-        std::error_code ec;
-        if (!(std::filesystem::exists(Database))) {
-            std::filesystem::create_directory(Database, ec);
-            std::filesystem::path dir = "./LocalDataStore/" + name;
-            if (!(std::filesystem::exists(dir))) {
-                std::ofstream file(dir);
+    inline bool GetLocalDataStore(const std::string& name) {
+        try {
+            std::filesystem::path Database = "./LocalDataStore";
+            std::error_code ec;
+            if (!(std::filesystem::exists(Database))) {
+                std::filesystem::create_directory(Database, ec);
+                std::filesystem::path dir = "./LocalDataStore/" + name;
+                if (!(std::filesystem::exists(dir))) {
+                    std::ofstream file(dir);
+                    return true;
+                }
+            } else {
+                std::filesystem::path dir = "./LocalDataStore/" + name;
+                if (!(std::filesystem::exists(dir))) {
+                    std::ofstream file(dir);
+                    return true;
+                }
             }
-        } else {
-            std::filesystem::path dir = "./LocalDataStore/" + name;
-            if (!(std::filesystem::exists(dir))) {
-                std::ofstream file(dir);
-            }
+            return false;
+        } catch(...) {
+            return false;
         }
     }
 
-    inline void SetAsync(const std::string& DataStoreName, const auto& str) {
-        std::filesystem::path dir = "./LocalDataStore/" + DataStoreName;
-        if (std::filesystem::exists(dir) && std::filesystem::is_regular_file(dir)) {
-            std::ofstream file(dir);
-            if (file.is_open()) {
-                file << str;
-                file.close();
+    inline bool SetAsync(const std::string& DataStoreName, const auto& str) {
+        try {
+            std::filesystem::path dir = "./LocalDataStore/" + DataStoreName;
+            if (std::filesystem::exists(dir) && std::filesystem::is_regular_file(dir)) {
+                std::ofstream file(dir);
+                if (file.is_open()) {
+                    file << str;
+                    file.close();
+                    return true;
+                }
             }
+            return false;
+        } catch (...) {
+            return false;
         }
     }
 
-    inline std::string GetAsync(const std::string& DataStoreName) {
+    struct GetAsyncResult {
+        bool found;
+        std::string data;
+    };
+    inline GetAsyncResult GetAsync(const std::string& DataStoreName) {
         std::filesystem::path dir = "./LocalDataStore/" + DataStoreName;
         if (std::filesystem::exists(dir) && std::filesystem::is_regular_file(dir)) {
             std::ifstream input(dir);
             if (input.is_open()) {
                 std::string line;
                 std::getline(input, line);
-                return line;
+                return {true,line};
             }
         }
-        return "";
+        return {false,""};
     }
 
-    inline void RemoveAsync(const std::string& DataStoreName) {
-        std::filesystem::path dir = "./LocalDataStore/" + DataStoreName;
-        if (std::filesystem::exists(dir) && std::filesystem::is_regular_file(dir)) {
-            std::filesystem::remove(dir);
+    inline bool RemoveAsync(const std::string& DataStoreName) {
+        try {
+            std::filesystem::path dir = "./LocalDataStore/" + DataStoreName;
+            if (std::filesystem::exists(dir) && std::filesystem::is_regular_file(dir)) {
+                std::filesystem::remove(dir);
+                return true;
+            }
+            return false;
+        } catch(...) {
+            return false;
+        }
+    }
+    inline bool RenameAsync(const std::string& DataStoreName, const std::string& Renamestring) {
+         try {
+            std::filesystem::path dir = "./LocalDataStore/" + DataStoreName;
+            std::filesystem::path renamed = "./LocalDataStore/" + Renamestring;
+            if (std::filesystem::exists(dir) && std::filesystem::is_regular_file(dir)) {
+                std::filesystem::rename(dir, renamed);
+                return true;
+            }
+            return false;
+        } catch(...) {
+            return false;
         }
     }
 };
+
+namespace filel {
+    struct FindFirstChildResult {
+        bool found;
+        std::filesystem::path path;
+    };
+
+    inline FindFirstChildResult FindFirstChild(const std::filesystem::path& Parentfolder, const std::string& Filename) {
+        if (!std::filesystem::exists(Parentfolder)) {
+            return {false,{}};
+        }
+        std::filesystem::path Childfile = Parentfolder / Filename;
+        if(std::filesystem::exists(Childfile)) {
+            return {true, Childfile};
+        }
+
+        return {false,{}};
+    }
+
+    inline bool Name(const std::filesystem::path& Filepath, const std::string NewName) {
+        if(!std::filesystem::exists(Filepath)) {
+            return false;
+        }
+        std::filesystem::path NewnameFile = Filepath.parent_path() / NewName;
+        std::filesystem::rename(Filepath, NewnameFile);
+        return true;
+    }
+    inline bool Destroy(const std::filesystem::path& Filepath) {
+        if(!std::filesystem::exists(Filepath)) {
+            return false;
+        }
+        if(std::filesystem::is_regular_file(Filepath)) {
+            std::filesystem::remove(Filepath);
+            return true;
+        } else if (std::filesystem::is_directory(Filepath)) {
+            std::filesystem::remove_all(Filepath);
+            std::filesystem::remove(Filepath);
+            return true;
+        }
+        return false;
+    }
+    
+}
 
 #endif
